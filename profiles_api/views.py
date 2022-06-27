eeebac76376506e09bd9c4f33adf9987fe1ab183
augustine-uba1 <augustine_ubanzemeka@yahoo.com>
2022-06-27 10:52:22 +0100

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -107,7 +107,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('name', 'email')
+    search_fields = ('name', 'email',)
 
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
